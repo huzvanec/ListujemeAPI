b83b6ef70c5116d70905e2f487343d00ab23644a
Mandlemankiller <mandlemankiller@email.cz>
2025-01-21 19:16:11 +0100

    update error handling

--- src/main/java/cz/jeme/programu/florianapi/rest/error/FlorianErrorController.java
+++ src/main/java/cz/jeme/programu/florianapi/rest/error/FlorianErrorController.java
@@ -1,28 +1,26 @@
 package cz.jeme.programu.florianapi.rest.error;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@ControllerAdvice
-public final class FlorianErrorController implements ErrorController {
-    private static final @NotNull Logger LOGGER = LoggerFactory.getLogger(FlorianErrorController.class);
-
+@RestController
+@RequestMapping("/error")
+public class FlorianErrorController implements ErrorController {
     private FlorianErrorController() {
         // bean
     }
 
-    @ExceptionHandler(Exception.class)
-    public @NotNull ResponseEntity<FlorianError> handle(final @NotNull Exception exception) {
-        if (exception instanceof final ErrorResponse response)
-            return new FlorianError(response.getStatusCode()).toResponse();
-        LOGGER.error("Unhandled exception", exception);
-        return new FlorianError(HttpStatus.INTERNAL_SERVER_ERROR).toResponse();
+    @GetMapping
+    public @NotNull ResponseEntity<FlorianError> handleError(final HttpServletRequest request) {
+        return new FlorianError(HttpStatusCode.valueOf(
+                (int) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE))
+        ).toResponse();
     }
 }
